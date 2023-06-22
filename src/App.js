73394d1ba1f0450f99c69607a4bd88084c4dc524
Phildallen <phil_372@live.com>
2023-06-22 15:27:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import BillSplitter from "./components/Amounts"
+import "./components/css/index.css"
+
+
+const App = () => {
+  return (
+    <>
+      <BillSplitter />
+      </>
+  )
+}
+
+export default App
