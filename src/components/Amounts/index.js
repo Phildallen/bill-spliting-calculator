5aa600a65ec8d0a2656d1ff8ec17c81f4a9d92b0
Phildallen <phil_372@live.com>
2023-07-04 17:34:57 +0100

    reset round up buttons added

--- src/components/Amounts/index.js
+++ src/components/Amounts/index.js
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import {NumericFormat} from 'react-number-format';
+import {useState} from "react"
+import {NumericFormat} from 'react-number-format'
 
 const BillSplitter = () => {
     const [bill, setBill] = useState(null)
@@ -8,6 +8,8 @@ const BillSplitter = () => {
     const [numOfPeople, setNumOfPeople] = useState(2)
     const [currency, setCurrency] = useState("£")
     const [view, setView] = useState('enterValues')
+    const [roundTotalCount, setRoundTotalCount] = useState(0)
+    const [roundTipCount, setRoundTipCount] = useState(0)
 
     function reset() {
         setBill(null)
@@ -17,9 +19,11 @@ const BillSplitter = () => {
         setView('enterValues')
     }
 
-    function calculate() {
+    const calculate = () => {
         if (bill > 0) {
             setView('showCalculation')
+            setRoundTotalCount(0)
+            setRoundTipCount(0)
         }
     }
 
@@ -39,13 +43,13 @@ const BillSplitter = () => {
     }
 
     const calculateTip = () => {
-        const value = (bill*100*tipPercentage)/10000
+        const value = (bill * 100 * tipPercentage) / 10000
         setTip(value.toFixed(2))
     }
 
     const handleTipDeduction = (e) => {
         if (tipPercentage > 0) {
-            setTipPercentage(tipPercentage -1)
+            setTipPercentage(tipPercentage - 1)
         }
         if (tipPercentage <= 0) {
             setTipPercentage(0)
@@ -54,7 +58,7 @@ const BillSplitter = () => {
 
     const handlePeopleDeduction = (e) => {
         if (numOfPeople > 2) {
-            setNumOfPeople(numOfPeople -1)
+            setNumOfPeople(numOfPeople - 1)
         }
         if (numOfPeople <= 2) {
             setNumOfPeople(2)
@@ -62,34 +66,65 @@ const BillSplitter = () => {
     }
 
     const roundUpTotal = () => {
-        const total = (parseFloat(bill) + parseFloat(tip)).toFixed(2)
-        if (total % 1 === 0) {
-            let value = (Math.ceil(total/10))*10
-            if (value-total > 5) {
-                setTip(((value - total - 5) + parseFloat(tip)).toFixed(2))
-            } else {
-                setTip(((value - total) + parseFloat(tip)).toFixed(2))
+        if (roundTotalCount + roundTipCount < 3) {
+            const total = (parseFloat(bill) + parseFloat(tip)).toFixed(2)
+            if (total % 10 === 0) {
+                let value = parseFloat(tip) + 10
+                setTip(parseFloat(value).toFixed(2))
+                setRoundTotalCount(roundTotalCount + 1)
+                return
             }
-        }
-        if (total % 1 !== 0) {
-            let value = Math.ceil(total)
-            setTip(((value-total) + parseFloat(tip)).toFixed(2))
+            if (total % 1 === 0) {
+                let value = (Math.ceil(total / 10)) * 10
+                if (value - total > 5) {
+                    setTip(((value - total - 5) + parseFloat(tip)).toFixed(2))
+                    setRoundTotalCount(roundTotalCount + 1)
+                    return;
+                } else {
+                    setTip(((value - total) + parseFloat(tip)).toFixed(2))
+                    setRoundTotalCount(roundTotalCount + 1)
+                    return;
+                }
+            }
+            if (total % 1 !== 0) {
+                let value = Math.ceil(total)
+                setTip(((value - total) + parseFloat(tip)).toFixed(2))
+                setRoundTotalCount(roundTotalCount + 1)
+            }
         }
     }
 
     const roundUpTip = () => {
-        const total = parseFloat(tip).toFixed(2)
-        if (total % 1 === 0) {
-            let value = (Math.ceil(total/10))*10
-            if (value - total > 5) {
-                setTip((value - 5).toFixed(2))
-            } else {
+        if (roundTotalCount + roundTipCount < 3) {
+            const total = parseFloat(tip).toFixed(2)
+            if (total % 1 === 0) {
+                let value = (Math.ceil(total / 10)) * 10
+                if (value - total > 5) {
+                    setTip((value - 5).toFixed(2))
+                    setRoundTipCount(roundTipCount + 1)
+                } else {
+                    setTip(value.toFixed(2))
+                    setRoundTipCount(roundTipCount + 1)
+                }
+            }
+            if (total % 1 !== 0) {
+                let value = Math.ceil(total)
                 setTip(value.toFixed(2))
+                setRoundTipCount(roundTipCount + 1)
             }
         }
-        if (total % 1 !== 0) {
-            let value = Math.ceil(total)
-            setTip(value.toFixed(2))
+    }
+
+    const undoRoundUpTotal = () => {
+        let count = roundTotalCount
+        setRoundTotalCount(0)
+        calculateTip()
+        if (roundTotalCount === 3) {
+            setRoundTotalCount(2)
+            // count = 2
+        }
+        if (count === 2) {
+            setRoundTotalCount(1)
         }
     }
 
@@ -102,77 +137,117 @@ const BillSplitter = () => {
                     <div className="billTotal">
                         <p>Bill Total</p>
                         <div className="bill">
-                        <select onChange={e => setCurrency(e.target.value) } name="currency" className={currency === 'CHF' ? "currency chf" : "currency"}>
-                            <option value="" selected disabled hidden>{currency}</option>
-                            <option value="£">£</option>
-                            <option value="€">€</option>
-                            <option value="$">$</option>
-                            <option value="CHF">CHF</option>
-                            <option value="Kč ">Kč</option>
-                            <option value="kr.">kr.</option>
-                            <option value="zł.">zł.</option>
-                        </select>
-                        <p onChange={handleBillChange}>
-                            <NumericFormat value={bill} decimalScale={2} fixedDecimalScale allowNegative={false}/>
-                        </p>
-                    </div>
+                            <select onChange={e => setCurrency(e.target.value)} name="currency"
+                                    className={currency === 'CHF' ? "currency chf" : "currency"}>
+                                <option value="" selected disabled hidden>{currency}</option>
+                                <option value="£">£</option>
+                                <option value="€">€</option>
+                                <option value="$">$</option>
+                                <option value="CHF">CHF</option>
+                                <option value="Kč ">Kč</option>
+                                <option value="kr.">kr.</option>
+                                <option value="zł.">zł.</option>
+                            </select>
+                            <p onChange={handleBillChange}>
+                                <NumericFormat value={bill} decimalScale={2} fixedDecimalScale allowNegative={false}/>
+                            </p>
+                        </div>
                     </div>
                     <p>Tip</p>
                     <div className="tipButtons">
-                        <button className={tipPercentage === 0 ? "tipSelected" : "tipButton"} onClick={() => setTipPercentage(0)}>0%</button>
-                        <button className={tipPercentage === 5 ? "tipSelected" : "tipButton"} onClick={() => setTipPercentage(5)}>5%</button>
-                        <button className={tipPercentage === 10 ? "tipSelected" : "tipButton"} onClick={() => setTipPercentage(10)}>10%</button>
+                        <button className={tipPercentage === 0 ? "tipSelected" : "tipButton"}
+                                onClick={() => setTipPercentage(0)}>0%
+                        </button>
+                        <button className={tipPercentage === 5 ? "tipSelected" : "tipButton"}
+                                onClick={() => setTipPercentage(5)}>5%
+                        </button>
+                        <button className={tipPercentage === 10 ? "tipSelected" : "tipButton"}
+                                onClick={() => setTipPercentage(10)}>10%
+                        </button>
                     </div>
                     <div className="tipButtons">
-                        <button className={tipPercentage === 15 ? "tipSelected" : "tipButton"} onClick={() => setTipPercentage(15)}>15%</button>
-                        <button className={tipPercentage === 20 ? "tipSelected" : "tipButton"} onClick={() => setTipPercentage(20)}>20%</button>
-                        <button className={tipPercentage === 25 ? "tipSelected" : "tipButton"} onClick={() => setTipPercentage(25)}>25%</button>
+                        <button className={tipPercentage === 15 ? "tipSelected" : "tipButton"}
+                                onClick={() => setTipPercentage(15)}>15%
+                        </button>
+                        <button className={tipPercentage === 20 ? "tipSelected" : "tipButton"}
+                                onClick={() => setTipPercentage(20)}>20%
+                        </button>
+                        <button className={tipPercentage === 25 ? "tipSelected" : "tipButton"}
+                                onClick={() => setTipPercentage(25)}>25%
+                        </button>
                     </div>
                     <div className="adjNumber">
-                    <button className="plusMinusButton" onClick={handleTipDeduction}> - </button>
-                    <span className="displayNumber"><span>{tipPercentage}%</span></span>
-                    <button className="plusMinusButton" onClick={() => setTipPercentage(tipPercentage+1)}> + </button>
-                </div>
+                        <button className="plusMinusButton" onClick={handleTipDeduction}> -</button>
+                        <span className="displayNumber"><span>{tipPercentage}%</span></span>
+                        <button className="plusMinusButton" onClick={() => setTipPercentage(tipPercentage + 1)}> +
+                        </button>
+                    </div>
 
                     <div>
-                    <p>People</p>
-                    <div className="adjNumber">
-                    <button className="plusMinusButton" onClick={handlePeopleDeduction}> - </button>
-                    <span className="displayNumber"><span>{numOfPeople}</span></span>
-                    <button className="plusMinusButton" onClick={() => setNumOfPeople(numOfPeople+1)}> + </button>
-                    </div>
+                        <p>People</p>
+                        <div className="adjNumber">
+                            <button className="plusMinusButton" onClick={handlePeopleDeduction}> -</button>
+                            <span className="displayNumber"><span>{numOfPeople}</span></span>
+                            <button className="plusMinusButton" onClick={() => setNumOfPeople(numOfPeople + 1)}> +
+                            </button>
+                        </div>
                     </div>
 
-                <div className="bottomButtons">
-                    <button className={bill > 0 ? "calculate" : "inactiveCalc"} onClick={() => {calculate(); calculateTip()}}>Calculate!</button>
-                                    <button className="reset" onClick={() => reset()}>Reset</button>
+                    <div className="bottomButtons">
+                        <button className={bill > 0 ? "calculate" : "inactiveLargeButton"} onClick={() => {
+                            calculate();
+                            calculateTip()
+                        }}>Calculate!
+                        </button>
+                        <button className="reset" onClick={() => reset()}>Reset</button>
+                    </div>
+
                 </div>
-
-            </div>
             </>
         )
     } else if (view === "showCalculation") {
         return (
             <>
                 <div className="content">
-                <div>
-                    <p>Total Per Person</p>
-                    <p className="calculation">{currency} {((parseFloat(bill) + parseFloat(tip))/numOfPeople).toFixed(2)}</p>
-                    {tipPercentage >0 ?<p>Bill/Tip Total</p> : <p>Bill Total</p>}
-                    {tipPercentage >0 ?<p className="calculation">{currency} {(parseFloat(bill) + parseFloat(tip)).toFixed(2)} / {currency} {tip}</p>
-                        : <p className="calculation">{currency} {(parseFloat(bill) + parseFloat(tip)).toFixed(2)}</p>}
-                    {tipPercentage >0 ?<p>Bill/Tip Per Person</p> : null}
-                    {tipPercentage >0 ?<p className="calculation">{currency} {((bill * 10000) / (numOfPeople * 10000)).toFixed(2)} /
-                        {currency} {((tip * 10000) / (numOfPeople * 10000)).toFixed(2)}</p> : null}
-                </div>
+                    <div>
+                        <p>Total Per Person</p>
+                        <p className="calculation">{currency} {((parseFloat(bill) + parseFloat(tip)) / numOfPeople).toFixed(2)}</p>
+                        {tip > 0 ? <p>Bill/Tip Total</p> : <p>Bill Total</p>}
+                        {tip > 0 ?
+                            <p className="calculation">{currency} {(parseFloat(bill) + parseFloat(tip)).toFixed(2)} / {currency} {tip}</p>
+                            :
+                            <p className="calculation">{currency} {(parseFloat(bill) + parseFloat(tip)).toFixed(2)}</p>}
+                        {tip > 0 ? <p>Bill/Tip Per Person</p> : null}
+                        {tip > 0 ?
+                            <p className="calculation">{currency} {((bill * 10000) / (numOfPeople * 10000)).toFixed(2)} /
+                                {currency} {((tip * 10000) / (numOfPeople * 10000)).toFixed(2)}</p> : null}
+                    </div>
                     <div className="bottomButtons">
-                    <button className="roundUpLarge" onClick={() => roundUpTotal()}>Round Up Total</button>
-                    <button className="roundUpLarge" onClick={() => roundUpTip()}>Round Up Tip</button>
-                        <button className="roundUpLarge" onClick={() => calculateTip()}>Reset Round Up</button>
-                    <button className="goBack" onClick={() => setView('enterValues')}>Go Back</button>
-                    <button className="reset" onClick={() => reset()}>Reset</button>
+                        <p>{roundTotalCount}</p>
+                        <button
+                            className={roundTotalCount + roundTipCount < 3 && roundTotalCount < 3 ? "roundUpLarge" : "roundUpLarge inactiveLargeButton"}
+                            onClick={() => roundUpTotal()}>Round Up Total
+                        </button>
+                        <button className={roundTotalCount > 0 ? "roundUpLarge" : "roundUpLarge inactiveLargeButton"}
+                                onClick={() => undoRoundUpTotal()}>Undo Rnd Up Total
+                        </button>
+
+                        <button
+                            className={roundTotalCount + roundTipCount < 3 && roundTipCount < 2 ? "roundUpLarge" : "roundUpLarge inactiveLargeButton"}
+                            onClick={() => roundUpTip()}>Round Up Tip
+                        </button>
+                        <button
+                            className={roundTotalCount + roundTipCount > 0 ? "roundUpLarge" : "roundUpLarge inactiveLargeButton"}
+                            onClick={() => {
+                                calculateTip();
+                                setRoundTotalCount(0);
+                                setRoundTipCount(0)
+                            }}>Reset Round Up
+                        </button>
+                        <button className="goBack" onClick={() => setView('enterValues')}>Go Back</button>
+                        <button className="reset" onClick={() => reset()}>Reset</button>
+                    </div>
                 </div>
-            </div>
             </>
         )
     }
